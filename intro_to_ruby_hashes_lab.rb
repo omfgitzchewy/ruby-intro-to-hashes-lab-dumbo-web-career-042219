a1c1544625945757e9a68c110205561130cd649f
Aaron Chu <aaronchu357@gmail.com>
2019-04-06 15:44:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly = {}
 	monopoly.merge!({railroads: {}})
-	monopoly[:railroads].merge!({pieces: 4})
+	monopoly[:railroads].merge!({:pieces => 4})
 end
 
 def monopoly_with_third_tier
